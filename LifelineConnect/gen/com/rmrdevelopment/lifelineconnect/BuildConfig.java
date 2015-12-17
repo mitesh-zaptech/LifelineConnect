bd631b58bbe8dc6c1f1f9de663ac80c8f71da186
Mitesh Zaptech <mitesh@zaptechsolutions.com>
2015-12-17 19:27:20 +0530

    New Commit 17th Dec 2015

--- LifelineConnect/gen/com/rmrdevelopment/lifelineconnect/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.rmrdevelopment.lifelineconnect;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- LifelineConnect/gen/com/rmrdevelopment/lifelineconnect/activities/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.rmrdevelopment.lifelineconnect.activities;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
